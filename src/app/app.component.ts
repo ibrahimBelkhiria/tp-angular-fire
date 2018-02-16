8b95f02cc9655c946c1febae44d27d2d547e35ae
ibrahim <ibrahim.belkhiria@episousse.com.tn>
2018-02-16 16:41:51 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+
+description: Observable<any>;
+
+messages: AngularFireList<any>;
+  email: string;
+  objet: string;
+  text: string;
+
+  msgs: Observable<any[]>;
+
+
+
+  constructor(private db: AngularFireDatabase) {
+
+    this.messages = db.list('messages').valueChanges();
+      console.log(this.messages);
+
+
+
+
+  }
+
+
+
+  addMessage(email, objet, text) {
+
+    console.log(this.email);
+    const  mes =  this.messages = this.db.list('messages');
+
+    mes.push(
+       {
+        email: this.email,
+        objet: this.objet,
+        text : this.text
+       });
+
+  }
+
+
+  deleteMesg() {
+    console.log('delet pressed');
+  }
+
+
+
+
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+
+const config = {
+  apiKey: "AIzaSyAa1I5rTye6KM_66ahFJXiFRbc-hoEqTvo",
+  authDomain: "tp-angularfire-d0423.firebaseapp.com",
+  databaseURL: "https://tp-angularfire-d0423.firebaseio.com",
+  projectId: "tp-angularfire-d0423",
+  storageBucket: "tp-angularfire-d0423.appspot.com",
+  messagingSenderId: "1066845910160"
+};
 
 
 @NgModule({
@@ -10,7 +21,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
